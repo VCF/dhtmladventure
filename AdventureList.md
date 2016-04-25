93f219bda7b563ab938709ee4e0646b28471fde5
Charles Tilford <github@biocode.fastmail.fm>
2016-04-24 23:38:02 -0400

    Some basic functionality working

--- AdventureList.md
+++ AdventureList.md
@@ -1,6 +1,28 @@
 # DHTML Adventure
 
+![Excalibur by HBKerr][MainImage]
+
+Currently testing Markdown parsing
+
 The following adventures are available:
 
-* TreasureHunt
+* [TreasureHunt](TreasureHunt)
+  * Level 2 list **BOLD TEXT**
+  * Level 2.b
 
+Text with `preformat chunk` embedded.
+
+    Code block
+    var potato = 20;
+
+
+### Credits
+
+* [jQuery JavaScript library][jQuery]
+* [jQuery User Interface][jQueryUI]
+* [markdown-js Markdown parser][markdownjs]
+
+[jQuery]: https://jquery.com/
+[jQueryUI]: https://jqueryui.com/
+[markdownjs]: https://github.com/evilstreak/markdown-js
+[MainImage]: image/excalibur_by_hbkerr.jpg "http://hbkerr.deviantart.com/art/Excalibur-107176451"
--- js/dhtmlAdventure.js
+++ js/dhtmlAdventure.js
@@ -2,7 +2,9 @@
 var DAMain;
 
 function DHTMLAdventure () {
-    this.layouts = {};
+    this.layouts    = {};
+    this.components = {};
+    this.objects    = {};
 }
 
 DHTMLAdventure.prototype.initLayout = function () {
@@ -24,13 +26,33 @@ DHTMLAdventure.prototype.initLayout = function () {
         },
         north: {
             // Image
-            minSize:     400
+            minSize:     300
         }
     });
+    // https://jqueryui.com/accordion/#sortable
+    this.layouts.objects = $( "#object-pane" ).accordion({
+        header: "> div > h3",
+        heightStyle: "content"
+    })
+        .sortable({
+            axis: "y",
+            handle: "h3",
+            stop: function( event, ui ) {
+          // IE doesn't register the blur when sorting
+          // so trigger focusout handlers to remove .ui-state-focus
+          ui.item.children( "h3" ).triggerHandler( "focusout" );
+ 
+          // Refresh accordion to handle new order
+          $( this ).accordion( "refresh" );
+        }
+      });
+
+    /* Tabs:
     $( "#object-pane" ).tabs()
         .addClass( "ui-tabs-vertical ui-helper-clearfix" );
     $( "#object-pane li" )
         .removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+    */
 }
 
 DHTMLAdventure.prototype.msg = function ( txt ) {
@@ -39,31 +61,92 @@ DHTMLAdventure.prototype.msg = function ( txt ) {
     
 }
 
-DHTMLAdventure.prototype.err = function ( txt ) {
-    
+DHTMLAdventure.prototype.objectSection = function ( id, header, text ) {
+    if (!id) return null;
+    if (this.objects[ id ]) return this.objects[ id ];
+    if (!header) header = id;
+    html = '<div class="group"><h3>' + header + '</h3><div id="'+id+'">';
+    if (text) html += text;
+    html += '</div>'; 
+    $( "#object-pane" ).append( html );
+    this.layouts.objects.accordion( "refresh" );
+    return this.objects[ id ] = $('#'+id);
+}
+
+DHTMLAdventure.prototype.debug = function ( html ) {
+    var obj = this.objectSection( 'DADebug', 'Code Debugging', 'Debugging messages:');
+    obj.append( '<div class="dbgMsg">'+html+"</div>" );
+}
+
+DHTMLAdventure.prototype.err = function ( html ) {
+    var obj = this.objectSection( 'DAErrors', 'Code Errors!', 'DHTML Adventure is having problems. They are listed below:');
+    obj.append( '<div class="errMsg">'+html+"</div>" );
 }
 
 DHTMLAdventure.prototype.init = function () {
     var startDoc = "AdventureList.md";
+    // this.debug(startDoc);
     this.parseMarkdownURI( startDoc );
-    // this.msg( startDoc );
+}
+
+var entityMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+};
+DHTMLAdventure.prototype.escXML = function ( xml ) {
+    // https://stackoverflow.com/a/12034334
+    return String(xml).replace(/[&<>"'\/]/g, function (s) {
+        return entityMap[s];
+    });
 }
 
 DHTMLAdventure.prototype.parseMarkdown = function ( text ) {
+    // https://github.com/evilstreak/markdown-js
     var tree = markdown.parse( text );
+    return tree;
 }
 
-DHTMLAdventure.prototype.parseMarkdownAJAX = function ( text, context ) {
-    this.msg( JSON.stringify( text ) );
+DHTMLAdventure.prototype.parseMarkdownAJAX = function ( text, reqType ) {
+    var tree = this.parseMarkdown(text);
+    if (reqType == "AdventureList") {
+        return this.renderAdventureList(tree);
+    }
+    // this.debug( JSON.stringify( tree ) );
+}
+
+DHTMLAdventure.prototype.renderAdventureList = function ( tree ) {
+    // Should we try to manipulate the tree in 
+    var jsonML = markdown.toHTMLTree( tree );
+    this.debug( "<pre>" + JSON.stringify( jsonML, null, "  " )+"</pre>" );
+    var html = markdown.toHTML( tree );
+    $("#main-pane").html( html )
+    // this.debug( "<pre>" + this.escXML( html) +"</pre>" );
 }
 
 DHTMLAdventure.prototype.parseMarkdownURI = function ( request ) {
     var self = this;
-    var context = "foo";
+    var reqType = "foo";
+    if (/AdventureList\.md/.test(request)) {
+        reqType = "AdventureList";
+    }
     $.ajax({url: request,
+            dataType: "text",
+            beforeSend: function(xhr){
+                // Silence "not well-formed" messages
+                // https://stackoverflow.com/a/4234006
+                if (xhr.overrideMimeType) xhr.overrideMimeType("text/text");
+            },
             success: function(result) {
-                self.parseMarkdownAJAX( result, context );
-            }});
+                self.parseMarkdownAJAX( result, reqType );
+            },
+            error: function(jqXHR, errType, errTxt) {
+                self.err("<b>"+reqType+"</b> : <i>"+errType+"</i> = "+errTxt);
+            }
+           });
 }
 
 
