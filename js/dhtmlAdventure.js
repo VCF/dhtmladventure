92346ba2c48588c63904e12e0272d7c88604da68
Charles Tilford <github@biocode.fastmail.fm>
2016-04-26 06:56:30 -0400

    room links appear to be working

--- js/dhtmlAdventure.js
+++ js/dhtmlAdventure.js
@@ -6,7 +6,7 @@ function DHTMLAdventure () {
     this.components = {};
     this.objects    = {};
     this.roomURIs   = {};
-    this.roomCache  = [];
+    this.roomCache  = [0];
 }
 
 DHTMLAdventure.prototype.initLayout = function () {
@@ -34,7 +34,8 @@ DHTMLAdventure.prototype.initLayout = function () {
     // https://jqueryui.com/accordion/#sortable
     this.layouts.objects = $( "#object-pane" ).accordion({
         header: "> div > h3",
-        heightStyle: "content"
+        heightStyle: "content",
+        collapsible: true
     })
         .sortable({
             axis: "y",
@@ -91,6 +92,11 @@ DHTMLAdventure.prototype.err = function ( html ) {
 
 DHTMLAdventure.prototype.init = function () {
     var startDoc = "AdventureList.md";
+    var dl = document.location;
+    if (dl.search) {
+        var m = dl.search.match(/^\?(.+\.md)$/)
+        if (m) startDoc = m[1];
+    }
     // this.debug(startDoc);
     var room = this.roomAction( startDoc, 'show' );
     if (room) room.show();
@@ -119,9 +125,14 @@ DHTMLAdventure.prototype.parseMarkdown = function ( text ) {
 }
 
 DHTMLAdventure.prototype.normalizeURI = function ( uri ) {
-    // TO DO - normalize URIs
-    // eg: ./foo.md -> foo.md, this//that.md -> this/that.md
-    // Absolute URIs?
+    if (/^(\/|http|file)/.test(uri)) {
+        // Just tidy absolute URIs
+    } else if (this.rootPath) {
+        uri = uri.replace(/\/{2,}/g, '/');
+        uri = uri.replace(/\/\.\//g, '/');
+        uri = uri.replace(/^\.\//, '');
+        uri = this.rootPath + uri;
+    }
     return uri;
 }
 
@@ -143,19 +154,20 @@ DHTMLAdventure.prototype.createRoom = function ( text, uri, action  ) {
         this.err("Room '"+uri+"' already exists at index "+ind)
         return room;
     }
-    this.roomCache.push( room );
     uri  = this.normalizeURI( uri );
     room = new DARoom( this );
+    this.roomCache.push( room );
     room.markdown = text;
     room.rawTree  = markdown.toHTMLTree( text );
     room.uri      = uri;
-    room.cache    = this.roomURIs[ uri ] = this.roomCache.length;
+    room.cache    = this.roomURIs[ uri ] = this.roomCache.length - 1;
     if (action) return room[ action ]();
     return room;
     // this.debug( JSON.stringify( tree ) );
 }
 
 DHTMLAdventure.prototype.roomAction = function ( uri, action ) {
+    uri = this.normalizeURI( uri );
     var cacheInd = this.roomURIs[ uri ];
     if (cacheInd) {
         var room = this.roomCache[ cacheInd ];
@@ -165,17 +177,20 @@ DHTMLAdventure.prototype.roomAction = function ( uri, action ) {
     return 0;
 }
 
-DHTMLAdventure.prototype.showRoom = function ( uri, recursing ) {
-    var room = this.roomAction( uri );
-    if (room) {
-        room.show();
-    }
-}
-
-
 DHTMLAdventure.prototype.retrieveURI = function ( uri, action, afterAction ) {
     var self = this;
     if (!action) action = "createRoom";
+    if (!this.rootPath) {
+        // https://stackoverflow.com/a/6644749
+        var temp = document.createElement('a');
+        temp.href = uri;
+        var root = uri = temp.href;
+        root = root.replace(/[^\/]+$/,"");
+        this.rootPath = root;
+        // this.debugObj( "Root set: "+root );
+    }
+    uri = this.normalizeURI(uri);
+    // alert(uri);
     $.ajax({url: uri,
             dataType: "text",
             beforeSend: function(xhr){
@@ -187,7 +202,8 @@ DHTMLAdventure.prototype.retrieveURI = function ( uri, action, afterAction ) {
                 self[ action ]( result, uri, afterAction );
             },
             error: function(jqXHR, errType, errTxt) {
-                self.err("<b>"+errType+"</b> : "+errTxt);
+                self.err("<b>"+errType+"</b> : While recovering '"+uri+
+                         "': <i>"+errTxt+"</i>");
             }
            });
 }
@@ -211,12 +227,18 @@ DARoom.prototype.type = function () {
     if (!t) {
         t = "Unknown";
         var uri = this.uri;
-        if (/AdventureList\.md/.test(uri)) {
+        if (/AdventureList\.md$/.test(uri)) {
             t = "AdventureList";
+        } else if (/\.md$/.test(uri)) {
+            t = "RoomFile";
         }
         this.roomType = t;
     }
     return t;
+}
+
+DARoom.prototype.noOp = function () {
+    // Empty function used for precaching
 }
 
 DARoom.prototype.show = function () {
@@ -230,23 +252,54 @@ DARoom.prototype.show = function () {
     }
 }
 
-DARoom.prototype.showAdventureList = function ( ) {
+DARoom.prototype._showMarkdown = function ( ) {
     // Should we try to manipulate the tree in HTML or jsonML?
     var jsonML = this.processJsonML();
-    this.DA.debugObj( jsonML );
+    // this.DA.debugObj( jsonML );
     var html = markdown.renderJsonML( jsonML );
     $("#main-pane").html( html )
     this.addExtras();
     // this.debug( "<pre>" + this.escXML( html) +"</pre>" );
 }
 
+DARoom.prototype.showAdventureList = DARoom.prototype._showMarkdown;
+DARoom.prototype.showRoomFile = DARoom.prototype._showMarkdown;
+
 DARoom.prototype.addExtras = function ( ) {
     var nt = this.newTree;
     var mImg = nt.mainImage;
+    var self = this;
     if (mImg) {
         $("#image-pane").html("<img src='"+mImg.src+"' />");
         this._creditButton( "#image-pane", mImg.title, mImg.alt)
-        this.DA.debugObj( nt.mainImage );
+    } else {
+        $("#image-pane").html("");
+    }
+    for (var i = 0; i < nt.images.length; i++) {
+        // Deal with inline images
+        var id   = nt.images[i];
+        var pSel = "#"+id;
+        var img  = $(pSel + " > img");
+        var orig = img.attr('title');
+        this._creditButton( pSel, orig, img.attr('alt'))
+        img.attr("title", "");
+    }
+    for (var r = 0; r < nt.rLinks.length; r++) {
+        // Deal with room links
+        var rId  = nt.rLinks[r];
+        var anc  = $('#'+rId);
+        anc.addClass('room-button')
+            .click(function( event ) {
+                event.preventDefault();
+                var uri = event.target.getAttribute('href');
+                // alert(uri);
+                self.DA.roomAction( uri, 'show' );
+            });
+        // Let's make sure that room is precached while we're here
+        try {
+            self.DA.roomAction( anc.attr('href'), 'noOp' );
+        } catch (e) {
+        }
     }
 }
 
@@ -261,7 +314,7 @@ DARoom.prototype._creditButton = function ( obj, orig, cred) {
     if (!cred) cred = 'Image Source';
     $(obj).append("<a id='"+imgID+"' target='_blank' href='"+
                   orig+"'>"+cred+"</a>");
-    $('#'+imgID).button()
+    $('#'+imgID).button().addClass('cred-button');
     $('#'+imgID).hide();
     // https://stackoverflow.com/a/20563834
     $(document).on('mouseenter', obj, function () {
@@ -273,14 +326,18 @@ DARoom.prototype._creditButton = function ( obj, orig, cred) {
 
 
 DARoom.prototype.processJsonML = function ( ) {
-    this.newTree = { };
+    this.newTree = { 
+        images: [],
+        rLinks: [],
+    };
     return this.newTree.tree = this._parse( this.rawTree );
 }
 
-DARoom.prototype._parse = function ( node ) {
+DARoom.prototype._parse = function ( node, parent ) {
     if (!(node instanceof Array)) return node; // String
     var tag     = node[0]; // First entry is tag name, eg "ul" or "p"
-    var newNode = [ tag ]; 
+    var newNode = [ tag ];
+    var nt = this.newTree;
     var nl = node.length;
     var i  = 1;
     if (nl) {
@@ -298,19 +355,46 @@ DARoom.prototype._parse = function ( node ) {
              i++;
          }
     }
+    // Not having attributes on all nodes can be irritating
+    // Make sure every node has an attribute hash in the second position:
+    if (i == 1) newNode.push( {} );
+    var attr = newNode[1];
     // Loop over the remaining parts, if any
     var childCount = 0;
     for (var j = i; j < nl; j++) {
-        var kid = this._parse( node[j] );
+        var kid = this._parse( node[j], newNode );
         if (kid) {
             newNode.push(kid);
             childCount++;
         }
     }
-    if (tag == 'img' && !this.newTree.mainImage) {
-        // The first image should go in the image pane
-        this.newTree.mainImage = newNode[1];
-        return null;
+    if (tag == 'img') {
+        // Normalize image URLs
+        attr.src = this.DA.normalizeURI( attr.src );
+        if (!nt.mainImage) {
+            // The first image should go in the image pane
+            nt.mainImage = attr;
+            return null;
+        }
+        // Inline image. We will class the parent <p> object and
+        // anchor credit buttons on that.
+        nt.images.push( parent[1].id = "Image" + ++this.idCnt );
+        parent[1].class = "cred-p";
+        // parent[0] = 'div';
+        childCount++;
+    } else if (tag == 'a') {
+        var href = attr.href = this.DA.normalizeURI( attr.href );
+        if (/\.md$/.test(href) && !(/^(\/|http)/.test(href))) {
+            // This appears to be another room file
+            nt.rLinks.push(attr.id = "RoomLink" + ++this.idCnt);
+        } else {
+            // External link
+            attr.class='ext-lnk'
+            attr['_target'] = 'blank'
+        }
+        // this.DA.debugObj( newNode );
+       
     }
-    return newNode;
+    
+    return childCount ? newNode : null;
 }
